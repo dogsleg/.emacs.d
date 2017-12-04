b1e6361bebbc67704b703cdd31f66583713c621f
Lev Lamberov <dogsleg@debian.org>
2017-12-04 21:35:01 +0500

    Add it-eburg.com RSS

--- init.el
+++ init.el
@@ -821,7 +821,8 @@
          ("http://www.opennet.ru/opennews/opennews_all.rss" ru-foss)
          ("http://feeds.feedburner.com/org/LOR" ru-foss)
          ("http://feeds.feedburner.com/rus-linux/VAK" ru-foss)
-         ("http://urfu.ru/ru/?type=2" urfu)
+         ("http://it-eburg.com/rss/rss.xml" ekb)
+         ("http://urfu.ru/ru/?type=2" ekb)
          "http://lwn.net/headlines/rss"
          "https://yakking.branchable.com/blog/index.rss"
          "http://feeds.feedburner.com/Phoronix"
