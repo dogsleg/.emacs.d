d9acde4dbc4bfff95d3cbdc1f4742e08157f0d21
Lev Lamberov <dogsleg@debian.org>
2018-04-10 16:39:15 +0500

    Better setup for TeX

--- init.el
+++ init.el
@@ -1064,6 +1064,7 @@
 
 ;; Disable linum-mode in pdf-tools-mode
 (add-hook 'pdf-view-mode-hook (lambda () (linum-mode -1)))
+(add-hook 'pdf-view-mode-hook (lambda () (linum-relative-mode -1)))
 
 ;; Disable linum-mode in EWW
 (add-hook 'eww-after-render-hook (lambda () (linum-mode -1)))
@@ -1085,15 +1086,10 @@
   (setq pdf-view-resize-factor 1.1)
   ;; Automatically annotate highlights
   (setq pdf-annot-activate-created-annotations t)
-  (setq TeX-view-program-selection '((output-pdf "pdf-tools")))
-  (setq TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view")))
   ;; Use ordinary isearch
   :bind ("C-s" . isearch-forward))
 
 (pdf-tools-install)
-
-;; Revert PDF buffer after successful compilation of TeX file
-(add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;                ;;;;
@@ -1104,6 +1100,34 @@
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
 (setq-default TeX-master nil)
+
+(use-package tex-site
+  :mode ("\\.tex\\'" . latex-mode)
+  :config
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq-default TeX-master nil)
+  (add-hook 'LaTeX-mode-hook
+            (lambda ()
+              (rainbow-delimiters-mode)
+              (turn-on-reftex)
+              (setq reftex-plug-into-AUCTeX t)
+              (reftex-isearch-minor-mode)
+              (setq TeX-PDF-mode t)
+              (setq TeX-source-correlate-method 'synctex)
+              (setq TeX-source-correlate-start-server t)))
+
+;; Revert PDF buffer after successful compilation of TeX file
+(add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
+
+;; Use pdf-tools with auctex
+(add-hook 'LaTeX-mode-hook 'pdf-tools-install)
+(setq TeX-view-program-selection '((output-pdf "pdf-tools"))
+      TeX-source-correlate-start-server t)
+(setq TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view"))))
+
+;; Prompt for empty optional arguments in cite
+(setq reftex-cite-prompt-optional-args t)
 
 ;;;;;;;;;;;;;;;;;;;;;
 ;;;;             ;;;;
