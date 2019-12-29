38f9be5b37b28fe72e4d40d5053cedd252af0647
Lev Lamberov <dogsleg@debian.org>
2019-12-29 21:37:26 +0500

    Better formatting

--- init.el
+++ init.el
@@ -538,9 +538,7 @@
   :config
   (add-hook 'shell-mode-hook  'with-editor-export-editor)
   (add-hook 'term-exec-hook   'with-editor-export-editor)
-  (add-hook 'eshell-mode-hook 'with-editor-export-editor)
-  )
-
+  (add-hook 'eshell-mode-hook 'with-editor-export-editor))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;                 ;;;;
