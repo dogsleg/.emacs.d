ff79403b0ef4bc1cba8af22ba76b1fe8edc16562
Lev Lamberov <dogsleg@debian.org>
2020-11-24 20:04:57 +0500

    Update custom.el

--- custom.el
+++ custom.el
@@ -4,21 +4,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("95b0bc7b8687101335ebbf770828b641f2befdcf6d3c192243a251ce72ab1692" "776af98f9708eba9756251b98ee26be3e964ce74777e12c5fab284a2f6195024" "e52718d4b950106873fed00c469941ad8db20f02392d2c7ac184c6defe37ad2c" "3f4ce0c77e72f5448039d25c095a4b1b1c0c82598104ad5336d07f9526d58628" "e9df267a1c808451735f2958730a30892d9a2ad6879fb2ae0b939a29ebf31b63" "688b48752d2002324e8b2e3bdea2bee536a18b4111a8dcdacb5ec193fd3a3654" "97bd701d278f6e103fe0cbe2114f779b75c3f17d04b856811f47376accce7c7f" default)))
+   '("95b0bc7b8687101335ebbf770828b641f2befdcf6d3c192243a251ce72ab1692" "776af98f9708eba9756251b98ee26be3e964ce74777e12c5fab284a2f6195024" "e52718d4b950106873fed00c469941ad8db20f02392d2c7ac184c6defe37ad2c" "3f4ce0c77e72f5448039d25c095a4b1b1c0c82598104ad5336d07f9526d58628" "e9df267a1c808451735f2958730a30892d9a2ad6879fb2ae0b939a29ebf31b63" "688b48752d2002324e8b2e3bdea2bee536a18b4111a8dcdacb5ec193fd3a3654" "97bd701d278f6e103fe0cbe2114f779b75c3f17d04b856811f47376accce7c7f" default))
  '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-folding elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
+   '(elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-folding elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
  '(notmuch-saved-searches
-   (quote
-    ((:name "unread" :query "tag:inbox AND tag:unread")
+   '((:name "unread" :query "tag:inbox AND tag:unread")
      (:name "inbox" :query "tag:inbox")
      (:name "local" :query "tag:local")
      (:name "debian-mailing-lists" :query "to:lists.debian.org")
-     (:name "bugs" :query "tag:bugs"))))
+     (:name "bugs" :query "tag:bugs")))
  '(package-selected-packages
-   (quote
-    (dh-elpa zzz-to-char ztree vimish-fold use-package undo-tree smex restart-emacs recursive-narrow rainbow-mode rainbow-delimiters powerline paredit org-bullets monokai-theme markdown-mode magit linum-relative iedit ido-vertical-mode ido-ubiquitous ibuffer-vc hydra git-timemachine flycheck fill-column-indicator expand-region eshell-up elfeed discover-my-major beacon async anzu agda2-mode ace-window ace-link))))
+   '(dh-elpa zzz-to-char ztree vimish-fold use-package undo-tree smex restart-emacs recursive-narrow rainbow-mode rainbow-delimiters powerline paredit org-bullets monokai-theme markdown-mode magit linum-relative iedit ido-vertical-mode ido-ubiquitous ibuffer-vc hydra git-timemachine flycheck fill-column-indicator expand-region eshell-up elfeed discover-my-major beacon async anzu agda2-mode ace-window ace-link)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
