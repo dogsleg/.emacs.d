36ed1a647fe181539899d2fb8711c394378f88ea
Lev Lamberov <dogsleg@debian.org>
2023-11-18 14:15:10 +0500

    Better org-roam handling

--- init.el
+++ init.el
@@ -1473,7 +1473,19 @@
   (setq org-bullets-bullet-list '("⊢" "⋮" "⋱" "⸳")))
 
 ;; Enable org-roam
-(use-package org-roam)
+(use-package org-roam
+  :init
+  (setq org-roam-2v-ack t)
+  :custom
+  (org-roam-directory "~/Documents/roam")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i" . completion-at-point))
+  :config
+  (org-roam-setup))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;                ;;;;
